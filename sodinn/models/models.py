d3842938a7a3ba37ebbc93bb40a2f8a1a58c78f4
Corentin <corentin.doco@phelma.grenoble-inp.fr>
2019-07-04 12:21:50 +0200

    debug

--- sodinn/models/models.py
+++ sodinn/models/models.py
@@ -259,7 +259,7 @@ class Model:
         filen_labdata = fh5.root.save_filename_labdata[0].decode() + '.hdf5'
         labeled_data = DataLabeler.load(filen_labdata)
         with tf.device('/cpu:0'):
-            model = models.load_model(filename +'.h5')
+            model = models.load_model(filename[0, -5] + '.h5')
 
         obj = cls(labeled_data)
         obj.model = model
